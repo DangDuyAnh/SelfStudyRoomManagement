400cbd5db882621d7b8d52fd9d8f12d9373b8c8f
vietanh2k <vietanha1k31@gmail.com>
2022-12-27 01:02:16 +0700

    add login

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
+import faceIcon from './res/facebook.png';
 import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm/>
     </div>
   );
 }
 
+class LoginForm extends React.Component{
+  render(){
+    return(
+      <div id="loginform">
+        <FormHeader title="Login" />
+        <Form />
+        <OtherMethods />
+      </div>
+    )
+  }
+}
+
+const FormHeader = props => (
+    <h2 id="headerTitle">{props.title}</h2>
+);
+
+
+const Form = props => (
+   <div>
+     <FormInput description="Username" placeholder="Enter your username" type="text" />
+     <FormInput description="Password" placeholder="Enter your password" type="password"/>
+     <FormButton title="Log in"/>
+   </div>
+);
+
+const FormButton = props => (
+  <div id="button" class="row">
+    <button>{props.title}</button>
+  </div>
+);
+
+const FormInput = props => (
+  <div class="row">
+    <label>{props.description}</label>
+    <input type={props.type} placeholder={props.placeholder}/>
+  </div>  
+);
+
+const OtherMethods = props => (
+  <div id="alternativeLogin">
+    <label>Or sign in with:</label>
+    <div id="iconGroup">
+      <Facebook />
+      <Google />
+    </div>
+  </div>
+);
+
+const Facebook = props => (
+  <a href="#" id="facebookIcon" ><img src={require('./res/facebook.png')} width="50" height="50"/> </a>
+);
+
+
+const Google = props => (
+  <a href="#" id="googleIcon"><img src={require('./res/google.png')} width="50" height="50"/></a>
+);
 export default App;
